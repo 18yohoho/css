30b8966dac0c4e308357c842aeb37b15847ce8a1
18yohoho <going-cuffs.0a@icloud.com>
2022-09-20 17:20:02 +0900

    add

--- include.js
+++ include.js
@@ -1,12 +1,19 @@
-$(function() {
-    $.ajax({
-      url: '/18yohoho.github.io/header.html',
-      dataType: 'html',
-      success: function (data) {
-        $('body').prepend(data);
-      },
-      error: function () {
-        alert('header error!');
-      },
-    });
+function header(){
+  $.ajax({
+      url: "/18yohoho.github.io/header.html",
+      cache: false,
+      success: function(html){
+          document.write(html);
+      }
   });
+}
+
+function footer(){
+  $.ajax({
+      url: "/18yohoho.github.io/footer.html",
+      cache: false,
+      success: function(html){
+          document.write(html);
+      }
+  });
+}
